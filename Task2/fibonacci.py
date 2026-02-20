0a396ffd05ea7d8a2159c83520bf31fb444aff05
L.K. van Mispelaar <lkmispelaar@virgo11.intra.astro.rug.nl>
2026-02-20 10:42:41 +0100

    week 2 updates

---
+++ Task2/fibonacci.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[10]:
+
+
+seq = [1, 1]
+while (len(seq) < 100):
+    seq == seq.append(int(seq[-1]) + int(seq[-2]))
+print(seq[99])
+
+
+# In[ ]:
+
+
+
+
